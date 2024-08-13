800253bc703f5188b135ea93f304e0e24a20b856
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-08-13 16:46:21 +0200

    end of day commit

--- frontend/src/pages/sspPlanning/SSPPlanning.jsx
+++ frontend/src/pages/sspPlanning/SSPPlanning.jsx
@@ -1,20 +1,68 @@
 export default function SSPPlanning() {
-  return (
-    <>
-      <div className="font-Effra_Hvy text-xl">
-        SSP Planning page <br />
-        <br />
-      </div>
-      <div className="font-Effra_Lt text-xl">Effra light font</div>
-      <div className="font-Effra_Rg text-xl">Effra regular font</div>
-      <div className="font-Effra_Md text-xl">Effra medium font</div>
-      <div className="font-Effra_Bd text-xl">Effra bold font</div>
-      <div className="font-Effra_Hvy text-xl">Effra heavy font</div>
+  const dateCol = [
+    "2024-08-12",
+    "2024-08-13",
+    "2024-08-14",
+    "2024-08-15",
+    "2024-08-16",
+    "2024-08-19",
+    "2024-08-20",
+    "2024-08-21",
+    "2024-08-22",
+    "2024-08-23",
+    "2024-08-26",
+    "2024-08-27",
+    "2024-08-28",
+    "2024-08-29",
+    "2024-08-30",
+    "2024-09-02",
+    "2024-09-03",
+    "2024-09-04",
+    "2024-09-05",
+    "2024-09-06",
+  ];
+  const employee1Tasks = [
+    { name: "Bakger001", numberOfDays: "3" },
+    { name: "Vakantie", numberOfDays: "4" },
+    { name: "10x kolomovens", numberOfDays: "2" },
+    { name: "Bakger002", numberOfDays: "5" },
+    { name: "Bakger003", numberOfDays: "5" },
+    { name: "vakantie", numberOfDays: "1" },
+  ];
+  const totalEmployees = 4;
 
-      <div className="btn btn-primary">primary color</div>
-      <div className="btn btn-secondary">secondary color</div>
-      <div className="btn btn-accent">accent color</div>
-      <div className="btn btn-neutral">neutral color</div>
-    </>
+  return (
+    <div className="flex">
+      <div className="grid-rows-[repeat(20, 20px)] grid h-[400px] auto-cols-[100px]">
+        {dateCol.map((item, index) => (
+          <div
+            key={index}
+            className={`bg-primary ${(index + 1) % 5 === 0 ? "mb-4" : ""} border-2 border-solid border-black`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid-rows-[repeat(20, 20px)] grid h-[400px] auto-cols-[200px]">
+        {employee1Tasks.map((item, taskIndex) =>
+          Array.from({ length: item.numberOfDays }).map((_, i) => {
+            const overallIndex =
+              employee1Tasks
+                .slice(0, taskIndex)
+                .reduce((acc, task) => acc + parseInt(task.numberOfDays), 10) +
+              i;
+            return (
+              <div
+                key={overallIndex}
+                className={`border-2 ${(overallIndex + 1) % 5 === 0 ? "mb-4" : ""} border-solid border-black bg-primary`}
+              >
+                {item.name}
+              </div>
+            );
+          }),
+        )}
+      </div>
+    </div>
   );
 }
