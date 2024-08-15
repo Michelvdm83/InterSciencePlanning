572f213baa25b5e8096702ebc8c8a3344ac83485
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-08-15 22:31:03 +0200

    Add holiday overview

---
+++ frontend/src/pages/holidays/components/Holiday.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Holiday({ holiday, holidays, setHolidays }) {
+  return (
+    <div className="flex justify-between rounded-md bg-base-100 p-3">
+      <div>
+        <p className="">
+          {holiday.startDate} <span className="mx-2">-</span> {holiday.endDate}
+        </p>
+      </div>
+
+      {/* <div className="">x</div> */}
+    </div>
+  );
+}
