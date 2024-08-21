52ae5503d49d60d3f5b43700508ad7b800bf3e06
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-08-21 21:54:35 +0200

    Refactor SystemService into reusable methods

--- backend/src/main/java/com/interscience/planning/system/SystemService.java
+++ backend/src/main/java/com/interscience/planning/system/SystemService.java
@@ -4,6 +4,7 @@ import com.interscience.planning.constructiontask.ConstructionTask;
 import com.interscience.planning.constructiontask.ConstructionTaskRepository;
 import com.interscience.planning.employee.Employee;
 import com.interscience.planning.employee.EmployeeRepository;
+import com.interscience.planning.employee.Function;
 import com.interscience.planning.exceptions.BadRequestException;
 import com.interscience.planning.exceptions.NotFoundException;
 import com.interscience.planning.ssptask.SSPTask;
@@ -25,21 +26,12 @@ public class SystemService {
   private final TestTaskRepository testTaskRepository;
 
   public void createNewSystem(SystemPostPatchDTO systemPostPatchDTO) {
-    if (systemRepository.findByName(systemPostPatchDTO.name()).isPresent()) {
-      throw new BadRequestException("System name already exists");
-    }
+    validateName(systemPostPatchDTO);
 
-    if (systemPostPatchDTO.name() == null || systemPostPatchDTO.name().trim().isEmpty()) {
-      throw new BadRequestException("System name is required");
-    }
     System system = new System(systemPostPatchDTO.name());
 
     if (systemPostPatchDTO.employeeResponsible() != null) {
-      Employee employeeResponsible =
-          employeeRepository
-              .findById(systemPostPatchDTO.employeeResponsible())
-              .orElseThrow(NotFoundException::new);
-      system.setEmployeeResponsible(employeeResponsible);
+      setEmployeeResponsible(systemPostPatchDTO, system);
     }
 
     system.setPoNumber(systemPostPatchDTO.poNumber());
@@ -66,21 +58,66 @@ public class SystemService {
     systemRepository.save(system);
   }
 
+  private void validateName(SystemPostPatchDTO systemPostPatchDTO) {
+    if (systemRepository.findByName(systemPostPatchDTO.name()).isPresent()) {
+      throw new BadRequestException("System name already exists");
+    }
+    if (systemPostPatchDTO.name() == null || systemPostPatchDTO.name().isBlank()) {
+      throw new BadRequestException("System name is required");
+    }
+  }
+
+  private void setEmployeeResponsible(SystemPostPatchDTO systemPostPatchDTO, System system) {
+    Employee employeeResponsible =
+        employeeRepository
+            .findById(systemPostPatchDTO.employeeResponsible())
+            .orElseThrow(NotFoundException::new);
+    if (employeeResponsible.getFunction() == Function.SSP) {
+      throw new BadRequestException("Employee responsible can't be an SSP employee");
+    }
+    system.setEmployeeResponsible(employeeResponsible);
+  }
+
+  private void setSSPEmployee(SystemPostPatchDTO systemPostPatchDTO, SSPTask sspTask) {
+    if (systemPostPatchDTO.estimatedConstructionDays() == null) {
+      throw new BadRequestException(
+          "Estimated construction days are required for assigning an SSP employee");
+    }
+    Employee sspEmployee =
+        employeeRepository
+            .findById(systemPostPatchDTO.employeeSSP())
+            .orElseThrow(NotFoundException::new);
+    if (sspEmployee.getFunction() != Function.SSP) {
+      throw new BadRequestException("SSP employee needs to have function SSP");
+    }
+    sspTask.setEmployee(sspEmployee);
+  }
+
+  private void setConstructionStartDate(
+      SystemPostPatchDTO systemPostPatchDTO, ConstructionTask constructionTask) {
+    if (systemPostPatchDTO.estimatedConstructionDays() == null) {
+      throw new BadRequestException(
+          "Estimated construction days required for setting construction start date");
+    }
+    if (systemPostPatchDTO.employeeSSP() == null) {
+      throw new BadRequestException("SSP employee required for setting construction start date");
+    }
+    constructionTask.setDateStarted(systemPostPatchDTO.startOfConstruction());
+  }
+
   private void createConstructionTask(SystemPostPatchDTO systemPostPatchDTO, System system) {
     ConstructionTask newConstructionTask = new ConstructionTask();
     SSPTask newSSPTask = new SSPTask();
 
-    if (systemPostPatchDTO.estimatedConstructionDays() != null) {
-      if (systemPostPatchDTO.employeeSSP() != null) {
-        Employee sspEmployee =
-            employeeRepository
-                .findById(systemPostPatchDTO.employeeSSP())
-                .orElseThrow(NotFoundException::new);
-        newSSPTask.setEmployee(sspEmployee);
-      }
-      newConstructionTask.setEstimatedTime(systemPostPatchDTO.estimatedConstructionDays());
+    if (systemPostPatchDTO.employeeSSP() != null) {
+      setSSPEmployee(systemPostPatchDTO, newSSPTask);
     }
-    newConstructionTask.setDateStarted(systemPostPatchDTO.startOfConstruction());
+
+    newConstructionTask.setEstimatedTime(systemPostPatchDTO.estimatedConstructionDays());
+
+    if (systemPostPatchDTO.startOfConstruction() != null) {
+      setConstructionStartDate(systemPostPatchDTO, newConstructionTask);
+    }
 
     newSSPTask.setConstructionTask(newConstructionTask);
     newConstructionTask.setSspTask(newSSPTask);
@@ -90,20 +127,44 @@ public class SystemService {
     system.setConstructionTask(newConstructionTask);
   }
 
+  private void setFTEmployee(SystemPostPatchDTO systemPostPatchDTO, TestTask testTask) {
+    if (systemPostPatchDTO.estimatedTestDays() == null) {
+      throw new BadRequestException(
+          "Estimated test days are required for assigning an FT employee");
+    }
+    Employee ftEmployee =
+        employeeRepository
+            .findById(systemPostPatchDTO.employeeFT())
+            .orElseThrow(NotFoundException::new);
+    if (ftEmployee.getFunction() != Function.FT) {
+      throw new BadRequestException("FT employee needs to have function FT");
+    }
+    testTask.setEmployee(ftEmployee);
+  }
+
+  private void setTestStartDate(SystemPostPatchDTO systemPostPatchDTO, TestTask testTask) {
+    if (systemPostPatchDTO.estimatedTestDays() == null) {
+      throw new BadRequestException("Estimated test days required for setting test start date");
+    }
+    if (systemPostPatchDTO.employeeFT() == null) {
+      throw new BadRequestException("FT employee required for setting test start date");
+    }
+    testTask.setDateStarted(systemPostPatchDTO.startOfTest());
+  }
+
   private void createTestTask(SystemPostPatchDTO systemPostPatchDTO, System system) {
     TestTask newTestTask = new TestTask();
 
-    if (systemPostPatchDTO.estimatedTestDays() != null) {
-      if (systemPostPatchDTO.employeeFT() != null) {
-        Employee ftEmployee =
-            employeeRepository
-                .findById(systemPostPatchDTO.employeeFT())
-                .orElseThrow(NotFoundException::new);
-        newTestTask.setEmployee(ftEmployee);
-      }
-      newTestTask.setEstimatedTime(systemPostPatchDTO.estimatedTestDays());
+    if (systemPostPatchDTO.employeeFT() != null) {
+      setFTEmployee(systemPostPatchDTO, newTestTask);
     }
-    newTestTask.setDateStarted(systemPostPatchDTO.startOfTest());
+
+    newTestTask.setEstimatedTime(systemPostPatchDTO.estimatedTestDays());
+
+    if (systemPostPatchDTO.startOfTest() != null) {
+      setTestStartDate(systemPostPatchDTO, newTestTask);
+    }
+
     newTestTask.setSystem(system);
     testTaskRepository.save(newTestTask);
     system.setTestTask(newTestTask);
