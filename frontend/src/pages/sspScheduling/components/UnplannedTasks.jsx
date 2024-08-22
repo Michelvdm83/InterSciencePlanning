8d9b464c0346d7411e80db88a682fe3c830c9a39
Michelvdm83 <michelvdm@gmail.com>
2024-08-22 14:22:23 +0200

    feedback verwerkt

--- frontend/src/pages/sspScheduling/components/UnplannedTasks.jsx
+++ frontend/src/pages/sspScheduling/components/UnplannedTasks.jsx
@@ -7,6 +7,7 @@ import PlannableTask from "./PlannableTask";
 export default function UnplannedTasks() {
   const [openTasks, setOpenTasks] = useState([]);
   const [employees, setEmployees] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     ApiService.get("employees/ssp-planning").then((response) => {
@@ -18,21 +19,37 @@ export default function UnplannedTasks() {
     });
   }, []);
 
+  function translateError(error) {
+    switch (error.toString()) {
+      case "Employee required":
+        return "Medewerker moet gekozen worden";
+      case "No enabled employee found":
+        return "Geen actieve medewerker gevonden";
+      default:
+        return "Er is een onbekende fout opgetreden. Probeer het later opnieuw.";
+    }
+  }
+
   function assignEmployee(event, taskId) {
     const newAssignee = event.target.value;
     if (newAssignee !== "") {
       ApiService.patch("ssptasks", {
         id: taskId,
         assignee: event.target.value,
-      }).then(() => {
-        const updatedOpenTasks = openTasks.filter((t) => t.id !== taskId);
-        setOpenTasks(updatedOpenTasks);
-      });
+      })
+        .then(() => {
+          const updatedOpenTasks = openTasks.filter((t) => t.id !== taskId);
+          setOpenTasks(updatedOpenTasks);
+        })
+        .catch((error) => {
+          setError(translateError(error.response?.data?.detail));
+        });
     }
   }
 
   return (
     <div className="flex flex-grow flex-col gap-2 rounded-md bg-neutral p-5">
+      {error && <p className="mt-4 text-red-600">{error}</p>}
       {openTasks.map((task) => {
         return (
           <PlannableTask
