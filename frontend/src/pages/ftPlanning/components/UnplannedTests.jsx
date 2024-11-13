647591f7a062502efc194b0b4bed4a92a8bb9d3e
Michelvdm83 <michelvdm@gmail.com>
2024-11-13 16:41:58 +0100

    Update UnplannedTests.jsx

--- frontend/src/pages/ftPlanning/components/UnplannedTests.jsx
+++ frontend/src/pages/ftPlanning/components/UnplannedTests.jsx
@@ -31,41 +31,45 @@ export default function UnplannedTasks({
         openTests.map((test) => {
           return (
             <div
-              className="flex w-3/4 cursor-pointer justify-between gap-2 overflow-clip rounded-md bg-white p-3"
+              className="flex max-h-16 w-3/4 cursor-pointer items-center gap-2 rounded-md bg-white p-3"
               key={test.id}
             >
-              <SystemModalButton
-                updateOpenTasks={updateOpenTests}
-                systemName={test.systemName}
-              >
-                <div className="w-1/2 truncate text-primary">
-                  {test.systemName}
-                </div>
-              </SystemModalButton>
-              {test.estimatedDays && (
-                <select
-                  defaultValue={""}
-                  onChange={(event) => assignEmployee(event, test.id)}
-                  className="select select-bordered select-accent select-sm w-1/2 cursor-pointer text-black"
-                  onClick={(event) => event.stopPropagation()}
+              <div className="w-1/2">
+                <SystemModalButton
+                  updateOpenTasks={updateOpenTests}
+                  systemName={test.systemName}
                 >
-                  <option value="" disabled>
-                    Naam
-                  </option>
-                  {employees.map((employee) => {
-                    return (
-                      <option value={employee.id} key={employee.id}>
-                        {employee.name}
-                      </option>
-                    );
-                  })}
-                </select>
-              )}
-              {!test.estimatedDays && (
-                <p className="w-1/2 text-wrap text-red-600">
-                  Productiedagen moet nog ingevuld worden
-                </p>
-              )}
+                  <div className="w-full overflow-hidden text-ellipsis text-primary">
+                    {test.systemName}
+                  </div>
+                </SystemModalButton>
+              </div>
+              <div className="w-1/2">
+                {test.estimatedDays && (
+                  <select
+                    defaultValue={""}
+                    onChange={(event) => assignEmployee(event, test.id)}
+                    className="select select-bordered select-accent select-sm cursor-pointer text-black"
+                    onClick={(event) => event.stopPropagation()}
+                  >
+                    <option value="" disabled>
+                      Naam
+                    </option>
+                    {employees.map((employee) => {
+                      return (
+                        <option value={employee.id} key={employee.id}>
+                          {employee.name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                )}
+                {!test.estimatedDays && (
+                  <p className="line-clamp-2 max-h-14 w-full text-ellipsis text-red-600">
+                    Testdagen moet nog ingevuld worden
+                  </p>
+                )}
+              </div>
             </div>
           );
         })}
