98552d957c35fd44a09b9debe0849fb894bf9274
Michelvdm83 <michelvdm@gmail.com>
2024-12-18 15:08:37 +0100

    only testtasks not done

--- backend/src/main/java/com/interscience/planning/testtask/TestTaskRepository.java
+++ backend/src/main/java/com/interscience/planning/testtask/TestTaskRepository.java
@@ -1,10 +1,13 @@
 package com.interscience.planning.testtask;
 
 import com.interscience.planning.employee.Employee;
+import com.interscience.planning.system.SystemStatus;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestTaskRepository extends JpaRepository<TestTask, UUID> {
   List<TestTask> findByEmployee(Employee employee);
+
+  List<TestTask> findByEmployeeAndSystem_StatusNot(Employee employee, SystemStatus status);
 }
