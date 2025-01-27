e03732f831e3bd2fd471721cad507e8b19b477b3
Michelvdm83 <michelvdm@gmail.com>
2025-01-27 13:18:33 +0100

    backend checklist

--- backend/src/main/java/com/interscience/planning/security/SecurityConfiguration.java
+++ backend/src/main/java/com/interscience/planning/security/SecurityConfiguration.java
@@ -2,13 +2,16 @@ package com.interscience.planning.security;
 
 import io.jsonwebtoken.Jwts;
 import javax.crypto.SecretKey;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -88,5 +91,10 @@ public class SecurityConfiguration {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(DataSource dataSource){
+    return new JdbcUserDetailsManager(dataSource);
   }
 }
