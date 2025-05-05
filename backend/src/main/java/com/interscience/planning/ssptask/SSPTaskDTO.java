5736684752c38dd7060212f807f505ad61300719
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2025-05-05 10:45:00 +0200

    added P.O. Number to the SSPTaskDTO response

--- backend/src/main/java/com/interscience/planning/ssptask/SSPTaskDTO.java
+++ backend/src/main/java/com/interscience/planning/ssptask/SSPTaskDTO.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 public record SSPTaskDTO(
     Integer index,
     String systemName,
+    String poNumber,
     String status,
     String taskName,
     UUID taskId,
@@ -19,12 +20,14 @@ public record SSPTaskDTO(
     LocalDate dateStarted = sspTask.getDateStarted();
     LocalDate dateCompleted = sspTask.getDateCompleted();
     String systemName = null;
+    String poNumber = null;
     String status = null;
     String taskName = null;
     UUID taskId = null;
     if (sspTask.getConstructionTask() != null) {
       System thisSystem = sspTask.getConstructionTask().getSystem();
       systemName = thisSystem == null ? null : thisSystem.getName();
+      poNumber = thisSystem == null ? null : thisSystem.getPoNumber();
       status = thisSystem == null ? null : thisSystem.getStatus().name();
     } else if (sspTask.getTask() != null) {
       taskName = sspTask.getTask().getName();
@@ -34,6 +37,7 @@ public record SSPTaskDTO(
     return new SSPTaskDTO(
         sspTask.getIndex(),
         systemName,
+        poNumber,
         status,
         taskName,
         taskId,
